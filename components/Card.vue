a1972fab3787fae09f6d9ca18766242e2fb717ff
SpellerBarbosa <133154923+SpellerBarbosa@users.noreply.github.com>
2025-04-09 16:54:57 -0300

    adicionado componente card

---
+++ components/Card.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Character from '~/model/Character';
+import CharacterCard from './CharacterCard.vue';
+
+defineProps<{
+    Characters: Character[]
+}>()
+</script>
+
+<template>
+    <div>
+        <Character 
+        v-for="(char, index) in Characters" 
+        :key="index" 
+        :character="char" />
+    </div>
+</template>
